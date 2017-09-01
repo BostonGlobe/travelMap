efdbfa188101457d5d0c3096da7b7a08156d2bc5
Yan Wu <yan.wu@globe.com>
2017-09-01 18:14:03 -0400

    Updated the marker and popup

--- src/Map.js
+++ src/Map.js
@@ -10,7 +10,7 @@ function Map() {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         });
 
-        _map.setView([42.323, -71.072], 8.5);
+        _map.setView([42.323, -71.572], 8.5);
         streetMap.addTo(_map);
 
         var arr = selection.datum()? selection.datum():[];
@@ -26,20 +26,31 @@ function Map() {
             shadowAnchor: [2, 31],  // the same for the shadow
             popupAnchor:  [-3, -38] // point from which the popup should open relative to the iconAnchor
         };
-        var greenIcon = L.icon(iconStyle);
+        var orangeZoom= {
+            iconUrl: './assets/leaf-red.png',
+            shadowUrl: './assets/leaf-shadow.png',
+            iconSize:     [38, 95],
+            shadowSize:   [50, 64],
+            iconAnchor:   [22, 94],
+            shadowAnchor: [4, 62],
+            popupAnchor:  [-3, -76]
+        };
+
+        var iconSmall = L.icon(iconStyle),
+            iconLarge = L.icon(orangeZoom);
 
         var markersLayer = L.layerGroup();
         var farmMarkers = Array.from(arr,function(d, i){
+            var popUp = L.popup()
+                .setContent("<p><a target='_blank' href='http://"+d.Website +"'><span> "+ d.LocationName+" </span></a><hr/><b>Address:</b> "+d.addresses+"<br/><b>Description:</b> "+d.Description+" </p>");
 
             var marker = L.marker([+d.lat, +d.lon],
-                {icon: greenIcon, riseOnHover: true})
-                .bindPopup("<p><a target='_blank' href='http://"+d.Website +"'><span> "+ d.LocationName+" </span></a><hr/><b>Address:</b> "+d.addresses+"<br/><b>Description:</b> "+d.Description+" </p>");
+                {icon: iconSmall, riseOnHover: true})
+                .bindPopup(popUp);
             markersLayer.addLayer(marker);
             var saveLatLng =L.latLng(42.323, -71.072);
 
             marker.on('click', function(e){
-                console.log(e.latlng);
-
                 if (_map.getZoom()<=12){
                     _map.flyTo(e.latlng, zoom);
                 } else if (saveLatLng == e.latlng){
@@ -49,10 +60,18 @@ function Map() {
                     _map.panTo(e.latlng);
                 }
                 saveLatLng= e.latlng;
-
             });
-
+            
+            _map.on('zoom', function (e) {
+                if (_map.getZoom()<12){
+                    marker.setIcon(iconSmall);
+                } else{
+                    marker.setIcon(iconLarge);
+                }
+                
+            });
         });
+
         markersLayer.addTo(_map);
         var overlay = {'markers': markersLayer};
        L.control.layers(null, overlay).addTo(_map);
