0f34d00e55e90708a5c9548c7f0b4db5f2f4a409
Yan Wu <yan.wu@globe.com>
2017-09-05 10:35:45 -0400

    Add locate function and new tile

--- src/Map.js
+++ src/Map.js
@@ -1,20 +1,30 @@
 function Map() {
     var _map;
+    var current_position, current_accuracy,
+        autoLocate=true;
     var exports = function(selection){
         var _farmData=[];
         console.log("Set the map");
         //map layer
         //light map https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaGVybWlvbmV3eSIsImEiOiJjaXZ5OWI1MTYwMXkzMzFwYzNldTl0cXRoIn0.Uxs4L2MP0f58y5U-UqdWrQ
-        var streetMap = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaGVybWlvbmV3eSIsImEiOiJjaXZ5OWI1MTYwMXkzMzFwYzNldTl0cXRoIn0.Uxs4L2MP0f58y5U-UqdWrQ', {
+        var satelliteMap = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaGVybWlvbmV3eSIsImEiOiJjaXZ5OWI1MTYwMXkzMzFwYzNldTl0cXRoIn0.Uxs4L2MP0f58y5U-UqdWrQ', {
             id: 'mapbox.street',
-            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            attribution: '&copy; <a href="http://osm.org/copyright">OpensatelliteMap</a> contributors'
+        });
+        var streetMap = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaGVybWlvbmV3eSIsImEiOiJjaXZ5OWI1MTYwMXkzMzFwYzNldTl0cXRoIn0.Uxs4L2MP0f58y5U-UqdWrQ', {
+            id: 'mapbox.street',
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
         });
 
         _map.setView([42.323, -71.572], 8.5);
-        streetMap.addTo(_map);
+        satelliteMap.addTo(_map);
 
+        var baseMaps = {
+            "Satellite": satelliteMap,
+            "Street": streetMap
+        };
         var arr = selection.datum()? selection.datum():[];
-        var zoom = 14;
+        var zoom = 15;
         //ADD ICON
 
         var iconStyle= {
@@ -42,7 +52,8 @@ function Map() {
         var markersLayer = L.layerGroup();
         var farmMarkers = Array.from(arr,function(d, i){
             var popUp = L.popup()
-                .setContent("<p><a target='_blank' href='http://"+d.Website +"'><span> "+ d.LocationName+" </span></a><hr/><b>Address:</b> "+d.addresses+"<br/><b>Description:</b> "+d.Description+" </p>");
+                .setContent("<p><span> "+ d.LocationName+
+                    " </span></p><p><b>Address:</b> "+d.addresses+"<br/><b>Website:</b> <a target='_blank' href='http://"+d.Website +"'>"+d.Website+"</a><br/><b>TEL:</b> "+ d.Phone+"</br><b>Description:</b> "+d.Description+" </p>");
 
             var marker = L.marker([+d.lat, +d.lon],
                 {icon: iconSmall, riseOnHover: true})
@@ -73,10 +84,33 @@ function Map() {
         });
 
         markersLayer.addTo(_map);
-        var overlay = {'markers': markersLayer};
-       L.control.layers(null, overlay).addTo(_map);
+        var overlay = {'Farms': markersLayer};
+       L.control.layers(baseMaps, overlay).addTo(_map);
+
+        L.easyButton('<img src="https://unpkg.com/leaflet@1.0.3/dist/images/marker-icon.png" width="15px"/>', function(btn, map){
+            _map.locate();
+            _map.on('locationerror', onLocationError);
+            _map.on('locationfound', onLocationFound);
+        }).addTo(_map);
+
+        function onLocationError(e) {
+            alert(e.message);
+        }
 
 
+        function onLocationFound(e) {
+            if (current_position) {
+                _map.removeLayer(current_position);
+                _map.removeLayer(current_accuracy);
+            }
+            var position = [e.latitude, e.longitude];
+
+                var radius = 100;
+                _map.setView(position, zoom);
+                current_position = L.marker(e.latlng).addTo(_map)
+                    .bindPopup("You are here.").openPopup();
+                current_accuracy = L.circle(e.latlng, radius).addTo(_map);
+        }
 
     }
     exports.map = function(_){
